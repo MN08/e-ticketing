6d805da15fbe00ad556ef47b91c1f0f1cb235972
Muhammad Nurhuda Hendrian <mnurhuda009@gmail.com>
2020-12-09 23:29:37 +0700

    change app name and add menu component

--- app/Http/Controllers/Dashboard/DashboardController.php
+++ app/Http/Controllers/Dashboard/DashboardController.php
@@ -14,6 +14,7 @@ class DashboardController extends Controller
      */
     public function index()
     {
+        // $active ='';
         return view('home');
     }
 
---
+++ app/View/Components/Menu.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\View\Components;
+
+use Illuminate\View\Component;
+
+class Menu extends Component
+{
+    /**
+     * Create a new component instance.
+     *
+     * @return void
+     */
+
+     public $active;
+
+    public function __construct($active)
+    {
+        $this->active = $active;
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     *
+     * @return \Illuminate\View\View|string
+     */
+    public function render()
+    {
+        $data = ['Dashboar','Home'];
+
+        return view('components.menu',['list'=>$data]);
+    }
+}
